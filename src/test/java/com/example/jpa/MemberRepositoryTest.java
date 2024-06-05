10476593270a62b2a3d596eee8ce2e7ca4eb4407
Tanguri <jungwontwo2@naver.com>
2024-06-05 16:50:58 +0900

    JPA와 DB 설정, 동작확인

---
+++ src/test/java/com/example/jpa/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.example.jpa;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    void save() {
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+
+//    @Test
+//    void find() {
+//    }
+}
