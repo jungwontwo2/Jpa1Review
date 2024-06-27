df40a8431e82c7672990d8689946f7abcca436fc
Tanguri <jungwontwo2@naver.com>
2024-06-27 15:56:36 +0900

    변경 감지와 병합

--- src/main/java/com/example/jpa/controller/ItemController.java
+++ src/main/java/com/example/jpa/controller/ItemController.java
@@ -59,17 +59,9 @@ public class ItemController {
         return "items/updateItemForm";
     }
 
-    @PostMapping("items/{itemId}/edit")
-    public String updateItem(@ModelAttribute("form") BookForm form){
-        Book book = new Book();
-        book.setId(form.getId());
-        book.setIsbn(form.getIsbn());
-        book.setName(form.getName());
-        book.setPrice(form.getPrice());
-        book.setStockQuantity(form.getStockQuantity());
-        book.setAuthor(form.getAuthor());
-        book.setIsbn(form.getIsbn());
-        itemService.saveItem(book);
+    @PostMapping("/items/{itemId}/edit")
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
         return "redirect:/items";
     }
 }
